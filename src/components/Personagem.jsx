c175fefc75ecea1be133089ae73314dd81092414
Jeferson C <jefersonblc@gmail.com>
2025-09-15 22:53:04 -0300

    Better labels

--- src/components/Personagem.jsx
+++ src/components/Personagem.jsx
@@ -485,18 +485,30 @@ function Personagem() {
         <div className="col-md">
           <label className="form-label"><i className="fas fa-heart"></i> Vida Total</label>
           <input type="number" name="vidatotal" className="form-control" title="Vida total (classe, con, nivel, extras...)" value={getLifeTotal()} onChange={handleChange} disabled />
-          <div className="input-group mt-1 gap-1">
-            <input type="number" name="vidaextralv" className="form-control" title="Vida extra por nível" value={personagem.vidaextralv} onChange={handleChange} />
-            <input type="number" name="vidaextra" className="form-control" title="Vida extra" value={personagem.vidaextra} onChange={handleChange} /> 
+          <div className="d-flex justify-content-between gap-1">
+            <div>
+              <label className="form-label form-label-sm">Vida extra por nível</label>
+              <input type="number" name="vidaextralv" className="form-control" title="Vida extra por nível" value={personagem.vidaextralv} onChange={handleChange} />
+            </div>
+            <div>
+              <label className="form-label form-label-sm">Vida extra</label>
+              <input type="number" name="vidaextra" className="form-control" title="Vida extra" value={personagem.vidaextra} onChange={handleChange} />
+            </div>
           </div>
         </div>
 
         <div className="col-md">
           <label className="form-label"><i className="fa-solid fa-flask"></i> Mana Total</label>
           <input type="number" name="manatotal" className="form-control" title="Mana total (classe, nivel, extras...)" value={getManaTotal()} onChange={handleChange} disabled />
-          <div className="input-group mt-1 gap-1">
-            <input type="number" name="manaextralv" className="form-control" title="Mana extra por nível" value={personagem.manaextralv} onChange={handleChange} />
-            <input type="number" name="manaextra" className="form-control" title="Mana extra" value={personagem.manaextra} onChange={handleChange} /> 
+          <div className="d-flex justify-content-between gap-1">
+            <div>
+              <label className="form-label form-label-sm">Mana extra por nível</label>
+              <input type="number" name="manaextralv" className="form-control" title="Mana extra por nível" value={personagem.manaextralv} onChange={handleChange} />
+            </div>
+            <div>
+              <label className="form-label form-label-sm">Mana extra</label>
+              <input type="number" name="manaextra" className="form-control" title="Mana extra" value={personagem.manaextra} onChange={handleChange} /> 
+            </div>
           </div>
         </div>
         
